5ca2f5f60d1443023b8dcfa8d85370224a08edd2
Flávia Correia <flavia.correia@usb.edu.br>
2026-07-21 11:56:08 -0300

    create initial html

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    //Open a window if none are open (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+
+})
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+
