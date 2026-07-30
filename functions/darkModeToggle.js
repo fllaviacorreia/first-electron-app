9c95710fb4dce7b49eb57eae9ba33b1f7f169d67
Flávia Correia <flavia.correia@usb.edu.br>
2026-07-30 13:46:34 -0300

    separate functions

---
+++ functions/darkModeToggle.js
@@ -0,0 +1,19 @@
+const { ipcMain, nativeTheme } = require('electron/main')
+
+const configDarkMode = () => {
+    ipcMain.handle('dark-mode:toggle', () => {
+        if (nativeTheme.shouldUseDarkColors) {
+            nativeTheme.themeSource = 'light'
+        } else {
+            nativeTheme.themeSource = 'dark'
+        }
+        return nativeTheme.shouldUseDarkColors
+    })
+
+    ipcMain.handle('dark-mode:system', () => {
+        nativeTheme.themeSource = 'system'
+    })
+}
+
+module.exports = { configDarkMode }
+
